fa20863f07509210692c560737c308ed5f340a45
Rongyi Chen <kenchenrong@gmail.com>
2022-11-11 17:19:46 -0800

    Use dirrent units for data

--- webapp/src/app/utils.ts
+++ webapp/src/app/utils.ts
@@ -29,11 +29,11 @@ export const capitalizeFirstLetter = (str: string) => {
 }
 
 export function formatBytes(bytes: number, decimals = 2) {
-    if (bytes === 0) return '0 Bytes';
+    if (bytes === 0) return '0 B';
 
-    const k = 1000;
+    const k = 1024;
     const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const sizes = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
 
     const i = Math.floor(Math.log(bytes) / Math.log(k));
 
