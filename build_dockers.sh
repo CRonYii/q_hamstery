d554a3fb9d2b414954501b8c1e5e2db54d8cbed4
Rongyi Chen <kenchenrong@gmail.com>
2022-12-06 18:39:12 +0000

    Update to 0.2.0 and impl build script

---
+++ build_dockers.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+ver=$( cat .version )
+echo "Building v$ver"
+
+# Update git submodule to latest
+cd backend/app
+git pull
+cd ../..
+cd nginx/webapp
+git pull
+cd ../
+# Build frontend webap artifacts
+./build.sh
+cd ..
+# Build and tag docker image
+sudo docker build ./backend/ -t q_hamstery_backend
+sudo docker image tag q_hamstery_backend cronyii/q_hamstery_backend:latest
+sudo docker image tag q_hamstery_backend cronyii/q_hamstery_backend:$ver
+sudo docker build ./nginx/ -t q_hamstery_nginx
+sudo docker image tag q_hamstery_nginx cronyii/q_hamstery_nginx:latest
+sudo docker image tag q_hamstery_nginx cronyii/q_hamstery_nginx:$ver
+# Push to Docker Hub
+sudo docker push cronyii/q_hamstery_nginx:latest
+sudo docker push cronyii/q_hamstery_nginx:$ver
+sudo docker push cronyii/q_hamstery_nginx:latest
+sudo docker push cronyii/q_hamstery_nginx:$ver
